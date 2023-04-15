7401b8abf4f346af7c543aa5171543e920572881
marquescleiton <cleiton.contato2210@gmail.com>
2023-04-15 11:21:51 -0300

    Branch vazia

--- spring-boot-task-blocker/src/main/java/br/com/marquescleiton/springboottaskblocker/jpa/ShedLockRepo.java
+++
@@ -1,9 +0,0 @@
-package br.com.marquescleiton.springboottaskblocker.jpa;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ShedLockRepo extends JpaRepository<ShedLockJpa, String>{
-	
-	public ShedLockJpa findByName(String nome);
-
-}
--- teste-exception/src/main/java/br/com/marquescleiton/testeexception/jpa/ClienteRepo.java
+++
@@ -1,7 +0,0 @@
-package br.com.marquescleiton.testeexception.jpa;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface ClienteRepo extends CrudRepository<ClienteJPA, String>{
-
-}
